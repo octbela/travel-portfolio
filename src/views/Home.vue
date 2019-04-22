b61ecfbeaa5ab0df5da71116c0d451d423d49abf
octbela <nabilahpermata6@gmail.com>
2019-04-22 16:01:17 +0700

    cek

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <v-btn color="success">Success</v-btn>
+    <v-btn color="success">Success</v-btn>
   </div>
 </template>
 
